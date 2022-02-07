a37acb295f60e9c965849c3bf21cb5a51607975b
Simon Vandaele <simonvdfr@users.noreply.github.com>
2022-02-07 13:46:38 +0100

    version op

--- import-overblog.php
+++ import-overblog.php
@@ -1,8 +1,9 @@
 <?php
-include_once("../../api/function.php");
 include_once("../../config.php");
+include_once("../../api/function.php");
+include_once("../../api/db.php");// Connexion à la db
 
-$xml = simplexml_load_file('export.xml','SimpleXMLElement', LIBXML_NOCDATA);//
+$xml = simplexml_load_file('export.xml','SimpleXMLElement', LIBXML_NOCDATA);//, LIBXML_NOCDATA
 $array = json_decode(json_encode((array)$xml), TRUE);
 
 //highlight_string(print_r($array['posts']['post'], true)); exit;
@@ -23,9 +24,9 @@ $array = json_decode(json_encode((array)$xml), TRUE);
 $i = 0;
 $num_total = count($array['posts']['post']);// Nombre total de fiche
 
-'Nombre d\'articles importés : '.$num_total;
+echo 'Nombre d\'articles importés : '.$num_total;
 
-$script_export = "REPLACE INTO `".$GLOBALS['db_prefix']."content` (`state`, `lang`, `robots`, `type`, `tpl`, `url`, `title`, `description`, `content`, `user_update`, `date_update`, `user_insert`, `date_insert`) VALUES\r\n";
+$script_export = "REPLACE INTO `".$GLOBALS['db_prefix']."content` (`id`, `state`, `lang`, `robots`, `type`, `tpl`, `url`, `title`, `description`, `content`, `user_update`, `date_update`, `user_insert`, `date_insert`) VALUES\r\n";
 
 foreach($array['posts']['post'] as $key => $val) 
 {
@@ -33,22 +34,42 @@ foreach($array['posts']['post'] as $key => $val)
     $content_fiche = array();
     $json_fiche = '';
 
-    print '<hr><h1 style="margin-bottom: 0;">' . $val['title'] .'</h1>';
-    print '📅 '.$val['created_at'].'<br>';
-    print '🔗 <a href="'.$val['slug'].'" target="_blank">'.$val['slug'].'</a>';
+
+    $id = 100+$key;
+
+    // Construction de l'url
+    $url = pathinfo($val['slug'], PATHINFO_FILENAME);
+    //$url = make_url($val['slug']);
+
+    echo '<hr><h1 style="margin-bottom: 0;">' . $val['title'] .'</h1>';
+
+    echo 'i:'.$i.'/key:'.$key.'/id:'.$id.'<br>';
+
+    if(!is_array($val['tags']))// Tags
+    {   
+        echo $val['tags'].'<br>';
+
+        $tags = explode(",", $val['tags']);
+        foreach($tags as $cle => $tag) {
+            //if(isset($tag) and $tag != "") $GLOBALS['connect']->query("REPLACE INTO ".$GLOBALS['tt']." SET id='".$id."', zone='actualites', encode='".encode($tag)."', name='".addslashes(trimer($tag))."', ordre='1'");
+        }
+    }
+
+    echo '📅 '.$val['created_at'].'<br>';
+    echo '🔗 <a href="'.$url.'" target="_blank">'.$url.'</a> | '.$val['slug'];
 
     $state = 'active';
 
-    $url = make_url($val['slug']);
+
 
 
     //-- début traitement contenu du post
 
-    //print '<h2>Ancienne DOM de l\'article</h2>';
-    //print '<code>'.@htmlspecialchars($val['content']).'</code>';
+    //echo '<h2>Ancienne DOM de l\'article</h2>';
+    //echo '<code>'.@htmlspecialchars($val['content']).'</code>';
 
-    print '<h3>contenu de l\'article</h3>';
-    print '<pre>';
+    echo '<h3>contenu de l\'article</h3>';
+    echo '<pre>';
     if(isset($val['content']) and !empty($val['content'])) 
     {
 
@@ -57,13 +78,13 @@ foreach($array['posts']['post'] as $key => $val)
         $content = preg_replace("/\r\n|\r|\n/", '<br/>', $content); //echappement retours lignes
         //$content = htmlentities($content,ENT_IGNORE,'UTF-8');
 
-        //print '<pre>'.htmlspecialchars($content).'</pre>';
+        //echo '<pre>'.htmlspecialchars($content).'</pre>';
 
 
         $dom_content = new DOMDocument;
-        $dom_content->loadHTML($content);
+        @$dom_content->loadHTML($content);
         
-        if(count($dom_content->getElementsByTagName('img')) == 0 ) print 'ℹ️ aucun visuel trouvé dans la dom de l\'article';
+        if(count($dom_content->getElementsByTagName('img')) == 0 ) echo 'ℹ️ aucun visuel trouvé dans la dom de l\'article';
 
         foreach($dom_content->getElementsByTagName('img') as $img) 
         {
@@ -89,42 +110,59 @@ foreach($array['posts']['post'] as $key => $val)
                 // réécriture des tailles
                 $new_width = 690;
 
-                if(file_exists($chemin.$dest_img)) print 'ℹ️ ';
+                if(file_exists($chemin.$dest_img)) echo 'ℹ️ ';
                 else 
                 {
                     if(copy($source_img, $chemin.$dest_img))
                     {
-                        // suppression de la hauteur
-                        if($img->hasAttribute('width')  && $img->getAttribute('width') > $new_width )  
+                        list($source_width, $source_height, $type, $attr) = getimagesize($chemin.$dest_img);
+
+                        // Si l'image est plus grande que la taille max
+                        if($img->hasAttribute('width') and $img->getAttribute('width') > $new_width)  
                         {    
-                            //@todo voir pour faire marcher le resize
-                            $chemin.$dest_img = resize($chemin.$dest_img, $new_width, null, 'article');
+                            $dest_img = resize($chemin.$dest_img, $new_width, null, 'article');
 
                             $content = str_replace(
                                 'width="'.$img->getAttribute('width').'"',
                                 'width="'.$new_width.'"',
                                 $content);
+
+                            echo 'width attr > new_width';
+                        }
+                        // Si l'image original est plus grande que la taille affiché
+                        else if($img->hasAttribute('width') and $source_width > $img->hasAttribute('width'))
+                        {    
+                            $dest_img = resize($chemin.$dest_img, $img->hasAttribute('width'), null, 'article');
+
+                            echo 'source_width > width attr';
+                        }
+                        // Si l'image original est plus grande que la largeur de l'article
+                        else if($source_width > $new_width)
+                        {    
+                            $dest_img = resize($chemin.$dest_img, $new_width, null, 'article');
+
+                            echo 'source_width > new_width';
                         }
 
                         // suppression de la hauteur
-                        if($img->hasAttribute('height'))  
+                        /*if($img->hasAttribute('height'))  
                         {
                             $content = str_replace(
                                 'height="'.$img->getAttribute('height').'"',
                                 '',
                                 $content);
-                        }
+                        }*/
 
-                        print '✅ ';
+                        echo ' ✅ ';
                     }
                     else
-                        print '❌ ';
+                        echo ' ❌ ';
 
                 }
 
                 echo'<a href="'.($source_img).'" target="_blank">'.($source_img).'</a> → '.($dest_img).'<br>';
 
-                $content = str_replace(($source_img), ($dest_img), $content);
+                $content = str_replace(($source_img), ($GLOBALS['path'].$dest_img), $content);
             }
 
         }
@@ -140,13 +178,13 @@ foreach($array['posts']['post'] as $key => $val)
         $json_fiche = json_encode($content_fiche, JSON_UNESCAPED_UNICODE);
     }
     else 
-        print 'ℹ️ aucun contenu dans l\'article';
+        echo 'ℹ️ aucun contenu dans l\'article';
     
     
-    print '</pre>';
+    echo '</pre>';
 
-    print '<h2>Nouvelle DOM de l\'article</h2>';
-    print '<code>'.@htmlspecialchars($content).'</code>';
+    //echo '<h2>Nouvelle DOM de l\'article</h2>';
+    //echo '<code>'.@htmlspecialchars($content).'</code>';
     
         
     //-- fin traitement contenu du post
@@ -154,16 +192,16 @@ foreach($array['posts']['post'] as $key => $val)
 
     //-- alimentation du tableau global (surement temporaire)
     $article = array(
-        'id' => '10'.$key,
+        'id' => $id,
         'state' => '\''.$state.'\'',
         'lang' => '\'fr\'',
         'robot' => '\'\'',
         'type' => '\'article\'',
         'tpl' => '\'article\'',
         'url' => '\''.$url.'\'',
-        'title' => '\''.$val['title'].'\'',
+        'title' => '\''.$GLOBALS['connect']->real_escape_string($val['title']).'\'',
         'description' => '\'\'',
-        'content' => '\''.($json_fiche).'\'',
+        'content' => '\''.$GLOBALS['connect']->real_escape_string($json_fiche).'\'',
         'user_update' => 2,
         'date_update' => '\''.$val['modified_at'].'\'',
         'user_insert' => 2,
@@ -183,16 +221,20 @@ foreach($array['posts']['post'] as $key => $val)
 
     //-- fin construction de la requete
 
-    //print '<pre style="max-width: 100%;">('.htmlspecialchars(implode( ",", $article )).')</pre>';
+    //echo '<pre style="max-width: 100%;">('.htmlspecialchars(implode( ",", $article )).')</pre>';
+
+    //if($i == 10) break;
 
 }
 
-//echo $script_export;
+//echo '<pre>'.htmlspecialchars($script_export).'</pre>';
+
+//$GLOBALS['connect']->query($script_export);
 
 //-- Début ecriture fichier sql
-    //$script_sql = fopen('script_export.sql', 'w');
-    //$script_sql = fopen('script_export.sql', 'c+b');
-    //fwrite($script_sql, $script_export);
+    $script_sql = fopen('script_export.sql', 'w');
+    $script_sql = fopen('script_export.sql', 'c+b');
+    fwrite($script_sql, $script_export);
 //-- Fin écriture fichier sql
 
 ?>
